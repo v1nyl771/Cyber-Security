27ffd2b83a4eb752c65d49c57061016bc13c8c8f
v1nyl771 <152433320+v1nyl771@users.noreply.github.com>
2024-11-21 11:18:49 -0400

    Create interactive.sh

---
+++ bash/interactive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Please enter your name:"
+read name
+echo ""
+
+echo "The even number parameters are:"
+echo "0: $0 2: $2 4: $4"
+echo ""
+
+echo "The odd number parameters are:"
+echo "1: $1 3: $3 5: $5"
+echo ""
+
+echo "The name you gave is: $name"
